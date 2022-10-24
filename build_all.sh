ca1cc0b7af9e25a13130a8b4901493da7adbe0d2
christinamanara <manara1024@gmail.com>
2022-10-24 18:16:16 +0300

    spark & hadoop runs

--- build_all.sh
+++ build_all.sh
@@ -6,8 +6,10 @@
 # Options argument scratch for deleting existing images and building the images from scratch
 
 if [ "$1" == scratch ]; then
-  docker rmi -f base hdfs-namenode hdfs-datanode 
+  docker rmi -f base hdfs-namenode hdfs-datanode spark-master spark-slave
 fi
 docker build -t base ./base/
 docker build -t hdfs-namenode ./hdfs-namenode/
 docker build -t hdfs-datanode ./hdfs-datanode/
+docker build -t spark-master ./spark-master/
+docker build -t spark-slave ./spark-slave/
