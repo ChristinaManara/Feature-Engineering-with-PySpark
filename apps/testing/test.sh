74066904bed76576b795ab9f5fe2a6e18305b67b
christinamanara <manara1024@gmail.com>
2022-10-25 23:05:17 +0300

    add data and new folders

--- apps/testing/test.sh
+++ apps/testing/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# Execute a simple example in Spark in order to confirm its installation 
 docker exec -it $(docker ps | grep feature-engineering-with-pyspark-spark-master-1 | awk '{print $1}') /bin/bash -c \
 "spark-submit --class org.apache.spark.examples.JavaSparkPi --master spark://spark-master-host:7077 /opt/spark-3.3.0-bin-hadoop3/examples/jars/spark-examples_2.12-3.3.0.jar"
